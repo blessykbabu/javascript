c76c9ddecb348d7af65d7c78a165197ba7e0b686
blessykbabu <blessykbabu5@gmail.com>
2023-09-12 16:12:37 +0530

    final yesterday

--- script.js
+++ script.js
@@ -1,1 +1,84 @@
 console.log("Hello , Welcome")
+var a=1;
+console.log("a",a);
+console.log('a',a);
+console.log("a 'b' c");
+console.log('b " hai" "helo"');
+console.log(`hai veena  welcome to "kochi"`)
+console.log(`helo 'good mornning'`)
+console.log(`${a}`);
+var b;
+b=1;
+console.log("b",b)
+b=10;
+console.log("b",b)
+var b=1;
+console.log("b",b)
+b=20;
+console.log("b",b)
+// let c;
+// console.log("c",c)
+// c=1;
+// console.log("c",c)
+// let c=10;
+// console.log("c",c)
+
+// const d;
+// console.log("d",d)
+const d=1;
+console.log("d",d)
+// const d=2;
+// console.log("d",d)
+let p,q;
+p=1
+q=2;
+console.log("p",p)
+console.log("q",q)
+let x=1 ,y=2;
+console.log("x",x)
+console.log("y",y)
+
+var f=1 ,g=2;
+console.log("f",f)
+console.log("g",g)
+
+let u,i;
+console.log("u",u,"i",i)
+
+let $b=a;
+console.log("$b",b)
+
+window.console.log("hai")
+
+ console.log("s",s)
+//  console.log("v",v)
+//  console.log("h",h)
+var s=6;
+// let v=3;
+// const h=4;
+
+console.log("s",s)
+//  console.log("v",v)
+//  console.log("h",h)
+
+ let z=2;
+ console.log("z",z);
+ let n="hai";
+ console.log(n);
+ let k=true;
+ console.log(k);
+ let l=null;
+ console.log(l);
+ let o;
+ console.log(o);
+ let a1=10,b1=20;
+ console.log(a1+b1);
+
+
+
+
+
+
+
+
+
