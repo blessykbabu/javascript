797bbe0a9e3c16594a8124c8c40450e760529f73
blessykbabu <blessykbabu5@gmail.com>
2023-09-13 16:49:23 +0530

    fuctions commited

--- script.js
+++ script.js
@@ -73,8 +73,65 @@ console.log("s",s)
  console.log(o);
  let a1=10,b1=20;
  console.log(a1+b1);
+ let c1=true;
+ let c2=false;
+ console.log("check",c1&&c2);
+ console.log(a1>b1);
+ console.log(b1>a1);
+ console.log(a1===b1);
+ console.log("a1=b1?",a1==b1);
+
+ let w1="hai";
+ let w2="hello";
+ console.log(w1+w2);
+ 
+ console.log(c1||c2);
 
 
+const string1="     welcome    ";
+console.log("length of the string",string1.length);
+const s1="uncopyrightable"
+console.log("slice",s1.slice(3));
+console.log("slice",s1.slice(3,9));
+console.log("slice3",s1.slice(-15,2));
+console.log("slice0",s1.slice(0));
+
+console.log("substring",s1.substring(3));
+console.log("substring0",s1.substring(0));
+console.log("substring-3",s1.substring(-3));
+
+
+console.log("sustr",s1.substr(3));
+console.log("slice",s1.slice(-9,-3));
+ console.log(c1!=0);
+ console.log("s",string1);
+ console.log(string1.trim());
+ console.log(string1.trimEnd());
+ console.log(string1.trimStart());
+
+let st="uncopyrightableaddjhkkkkkkkkkkjjjjjjjjjj";
+ console.log("split",st.split());
+
+
+let st1="uncopyrightablea,ddjhkkkkkkkk,kkjjjjjjjjjj";
+ console.log("split",st1.split(','));
+
+ let st2="uncopyrighta*bleaddjhkkkkkkkkkkj,jjjjjjjjj";
+ let s3=st2.split('*');
+ console.log("splitedd",s3);
+ 
+
+ console.log("join",s3.join('*'));
+ console.log("replace",string1.replace('we','ab'))
+ let n1="welcome"
+ console.log("endswith",n1.endsWith('me'));
+ console.log("startswith",n1.startsWith('we'));
+ console.log("startswith",n1.startsWith('ab'));
+ 
+
+ 
+
+ 
 
 
 
