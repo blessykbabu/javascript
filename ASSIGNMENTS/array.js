d37e79bf7e48ed5c6204a0d8ad2fc1b447ce474f
blessykbabu <blessykbabu5@gmail.com>
2023-09-14 17:36:02 +0530

    array properties added

--- ASSIGNMENTS/array.js
+++ ASSIGNMENTS/array.js
@@ -6,3 +6,23 @@ console.log("sort:",a.sort())
 
 let fruit = [arr.length - 1];
 console.log("Accessing the Last Array Element",fruit);
+//console.log("every",arr.every());
+console.log("reverse",arr.reverse());
+//console.log("some",arr.some(""));
+console.log("unshift",arr.unshift("potato"));
+console.log("valueof",arr.valueOf());
+//console.log("reduce",arr.reduceRight());
+let t=2;
+console.log("at()",arr.reverse(t));
+let concat=arr.concat(a);
+console.log("concat",concat);
+let cons=arr.constructor;
+console.log("constructor",arr.constructor());
+console.log("copy with in",arr.copyWithin(1,2));
+let f=arr.entries();
+console.log("entries",f);
+let inc =arr.includes("day");
+console.log("includes",inc);
+let result = Array.isArray(arr);
+console.log("isarray",result)
+
