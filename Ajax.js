f1cf805f74f88214b89fa6c1d53ab2376a0b07e3
blessykbabu <blessykbabu5@gmail.com>
2023-09-29 13:00:03 +0530

    ajax

---
+++ Ajax.js
@@ -0,0 +1,3 @@
+var xhr=new  XMLHttpRequest()
+xhr.open("get","https://jsonplaceholder.typicode.com/users")  //url from fake api
+xhr.send()
--- asychronous.js
+++ asychronous.js
@@ -1,8 +1,8 @@
-console.log("start")            //1st start 2nd end 
+console.log("start")            //1st console start. 2nd  console end then the settimeout 
 // setTimeout(()=>{
 // console.log("helo")
 // },0)
 
-fetch('https://jsonplaceholder.typicode.com/todos/1')
+fetch('https://jsonplaceholder.typicode.com/todos/1')  //like above
 .then(json => console.log(json))
 console.log("end")
